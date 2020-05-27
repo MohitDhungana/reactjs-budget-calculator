9a50dd35a2c29df8365df56c8e1e162e8a9fa042
MohitDhungana <mohitdhungana@yahoo.com>
2020-05-27 15:25:21 +0545

    initial commit with uuid

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ExpenseForm = () => {
+  return <div>this is expense form</div>;
+};
+
+export default ExpenseForm;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Item from './ExpenseItem';
+
+const ExpenseList = () => {
+  return (
+    <div>
+      this is expense list
+      <Item />
+    </div>
+  );
+};
+
+export default ExpenseList;
